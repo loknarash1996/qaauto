35f205bc07a46b69ac11247047824970864ba89e
loknarash1996 <163719078+loknarash1996@users.noreply.github.com>
2024-03-17 13:43:04 +0300

    переименовал 3.2 в 3_2

--- 3_2lastless.py
+++ 3_2lastless.py
@@ -7,6 +7,29 @@ import unittest
 value=["Да", "Конечно", "Нет", "Отнюдь", "Естественно", "Ясен-красен", "Вовсе нет!"]
 class TestAbs(unittest.TestCase):
     def test_abs1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        #link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+        firstname = browser.find_element(By.XPATH,"/html/body/div/form/div[1]/div[1]/input")
+        firstname.send_keys(random.choice(value))
+        lastname = browser.find_element(By.XPATH,"/html/body/div/form/div[1]/div[2]/input")
+        lastname.send_keys(random.choice(value))
+        email = browser.find_element(By.XPATH,"/html/body/div/form/div[1]/div[3]/input")
+        email.send_keys(random.choice(value))
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+    def test_abs2(self):
         link = "http://suninjuly.github.io/registration2.html"
         #link = "http://suninjuly.github.io/registration2.html"
         browser = webdriver.Chrome()
@@ -23,7 +46,6 @@ class TestAbs(unittest.TestCase):
 
         # Проверяем, что смогли зарегистрироваться
         # ждем загрузки страницы
-        time.sleep(1)
 
         # находим элемент, содержащий текст
         welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
